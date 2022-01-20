3b8b2e82aaed90b42b2456cced955fb2011c16fe
michimani <michimani210@gmail.com>
2022-01-20 23:00:18 +0900

    2.7.3

--- chapter2/quickreplace/src/main.rs
+++ chapter2/quickreplace/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::env;
 use std::fs;
 use text_colorizer::*;
@@ -26,7 +27,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text {:?}", "Error".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
@@ -67,4 +76,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
